56ff01732f4c475192c1305d685e01256bf467b3
cads <cadswetha@gmail.com>
2022-09-01 01:36:37 +0530

    static root

--- blog/settings.py
+++ blog/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
